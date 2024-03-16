ce4514db133a7445e8497c7ecb24d7a544a7b890
shellmarie <155805233+shellmarie@users.noreply.github.com>
2024-03-16 18:09:41 +0800

    Delete Tsa4/Assets directory

--- Tsa4/Assets/Js/script.js
+++
@@ -1,8 +0,0 @@
-function showFood(food) {
-    document.getElementById(food + "-info").style.display = "block";
-  }
-  
-  function hideFood(food) {
-    document.getElementById(food + "-info").style.display = "none";
-  }
-  
